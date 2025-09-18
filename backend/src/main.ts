a0b52b211abe0bf9697164d6ecef3c20f6e10381
jooyongyard <113870906+jooyongyard@users.noreply.github.com>
2025-09-18 16:06:17 +0900

    초기화, 다시 시작 ..

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,9 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // 전역 유효성 검사 파이프 추가
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
